7b9ea24dab95a82795117651a938423315ea4b5a
Filipe M. Silva <filipe@smartrips.co>
2018-06-09 08:02:04 -0300

    call getValidKeys to filter valid keys before get new object

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict'
 
+var getValidKeys = require('./lib/getValidKeys.js');
+
 function Translate(schema, json) {
-  
+
+  var clone = {},
+      key;
+      
+  var validKeys = getValidKeys(schema, json);
+
+  console.log(validKeys);
 }
 
 module.exports = Translate;
