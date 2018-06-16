c3dbaf69fcbfd5988efb568740c548c170088037
Filipe M. Silva <filipe@smartrips.co>
2018-06-16 19:28:01 -0300

    add recursive for JSON objects

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-'use strict'
-
-var exampleJson = require('./.example.json');
+'use strict';
 
 var validateSchema = require('./lib/validate-schema.js');
 var getValidKeys = require('./lib/get-valid-keys.js');
+var getRecursiveKeys = require('./lib/get-recursive-keys.js');
 var buildNewObject = require('./lib/build-new-object.js');
 
 function JsonModifier(schema, json) {
+  var validKeys, recursive, modifiedJSON;
+
   validateSchema(schema);
-  var validKeys = getValidKeys(json, schema);
-  return buildNewObject(validKeys, json, schema);
+
+  validKeys = getValidKeys(json, schema);
+  recursive = getRecursiveKeys(schema);
+  modifiedJSON = buildNewObject(validKeys, json, schema);
+
+  !!recursive.length && recursive.forEach(function (objKey) {
+    modifiedJSON[objKey] = JsonModifier(schema[objKey], json[objKey]);
+  });
+
+  return buildedObject;
 }
 
 module.exports = JsonModifier;
